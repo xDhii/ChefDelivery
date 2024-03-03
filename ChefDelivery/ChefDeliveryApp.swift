cf85bc81e515a3f7938720cd0f6f63e64a792cd4
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2024-03-03 12:03:57 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Adriano Valumin on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
