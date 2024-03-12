3462361908ba9ca7f3e0008ac408083316fd96af
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2024-03-11 21:34:07 -0300

    Added carousel auto scroll

--- ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -8,20 +8,34 @@
 import SwiftUI
 
 struct CarouselTabView: View {
+    @State private var currentIndex = 1
+
     let ordersMock: [OrderType] = [
-        OrderType(id: 0, name: "banner burger", image: "barbecue-banner"),
-        OrderType(id: 1, name: "banner prato feito", image: "brazilian-meal-banner"),
-        OrderType(id: 2, name: "banner poke", image: "pokes-banner"),
+        OrderType(id: 1, name: "banner burger", image: "barbecue-banner"),
+        OrderType(id: 2, name: "banner prato feito", image: "brazilian-meal-banner"),
+        OrderType(id: 3, name: "banner poke", image: "pokes-banner"),
     ]
 
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex) {
             ForEach(ordersMock) { order in
                 CarouselItemView(order: order)
+                    .tag(order.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+                withAnimation(.easeInOut(duration: 1)) {
+                    if currentIndex < ordersMock.count {
+                        currentIndex += 1
+                    } else {
+                        currentIndex = 1
+                    }
+                }
+            }
+        }
     }
 }
 
