a5014e7f2a94cb56500890ecc5102043b52b85f1
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2024-07-17 11:51:00 -0300

    Project reorganizaton

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -8,68 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
-        // MARK: Atrtibutes
+	// MARK: Atrtibutes
 
-    private var service = HomeService()
-    @State private var storesType: [StoreType] = []
-    @State private var isLoading: Bool = true
+	private var service = HomeService()
+	@State private var storesType: [StoreType] = []
+	@State private var isLoading: Bool = true
 
-        // MARK: View
+	// MARK: View
 
-    var body: some View {
-        NavigationStack {
-            VStack {
-                if isLoading {
-                    ProgressView()
-                } else {
-                    NavigationBar()
-                        .padding(.horizontal, 15)
-                    ScrollView(.vertical, showsIndicators: false) {
-                        VStack(spacing: 20) {
-                            OrderTypeGridView()
-                            CarouselTabView()
-                            StoresContainerView(stores: storesType)
-                        }
-                    }
-                }
-            }
-        }
-        .onAppear {
-//            Task {
-//                await getStores()
-//            }
-            getStoresWithAlamofire()
-        }
-    }
+	var body: some View {
+		NavigationStack {
+			VStack {
+				if isLoading {
+					ProgressView()
+				} else {
+					NavigationBar()
+						.padding(.horizontal, 15)
+					ScrollView(.vertical, showsIndicators: false) {
+						VStack(spacing: 20) {
+							OrderTypeGridView()
+							CarouselTabView()
+							StoresContainerView(stores: storesType)
+						}
+					}
+				}
+			}
+		}
+		.onAppear {
+			Task {
+				await getStores()
+			}
+			getStoresWithAlamofire()
+		}
+	}
 
-        // MARK: Methods
+	// MARK: Methods
 
-    func getStores() async {
-        do {
-            let result = try await service.fetchData()
-            switch result {
-                case .success(let stores):
-                    self.storesType = stores
-                case .failure(let error):
-                    print(error.localizedDescription)
-            }
-        } catch {
-            print(error.localizedDescription)
-        }
-        withAnimation {
-            self.isLoading = false
-        }
-    }
+	func getStores() async {
+		do {
+			let result = try await service.fetchData()
+			switch result {
+				case let .success(stores):
+					self.storesType = stores
+				case let .failure(error):
+					print(error.localizedDescription)
+			}
+		} catch {
+			print(error.localizedDescription)
+		}
+		withAnimation {
+			self.isLoading = false
+		}
+	}
 
-    func getStoresWithAlamofire() {
-        service.fetchDataWithAlamofire { stores, error in
-            guard let stores = stores else { return }
-            self.storesType = stores
-        }
-    }
-
+	func getStoresWithAlamofire() {
+		service.fetchDataWithAlamofire { stores, _ in
+		}
+	}
 }
 
 #Preview(traits: .fixedLayout(width: 400, height: 250)) {
-    ContentView()
+	ContentView()
 }
